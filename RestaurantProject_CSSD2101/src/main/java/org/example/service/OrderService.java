8a929b8032bc918166745bfac5fb1e8c2671b832
felda-del <felda@my.yorku.ca>
2025-12-15 23:11:44 -0500

    Delete RestaurantProject_CSSD2101/src/main/java/org/example directory

--- RestaurantProject_CSSD2101/src/main/java/org/example/service/OrderService.java
+++
@@ -1,6 +0,0 @@
-package org.example.service;
-
-// this is where alice's stuff should be i think
-
-public class OrderService {
-}
--- RestaurantProject_CSSD2101/src/main/java/org/example/service/PaymentService.java
+++
@@ -1,5 +0,0 @@
-package org.example.service;
-
-// this is where alice's stuff should be
-public class PaymentService {
-}

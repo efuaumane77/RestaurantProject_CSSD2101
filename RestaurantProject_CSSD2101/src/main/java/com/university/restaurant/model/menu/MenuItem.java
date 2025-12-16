2a6d1463d38a102c04a1ef42b5ad967b871ccbcb
Efua Umane <efuaumane79@gmail.com>
2025-12-16 13:38:33 -0500

    Update MenuItem.java
    comments

--- RestaurantProject_CSSD2101/src/main/java/com/university/restaurant/model/menu/MenuItem.java
+++ RestaurantProject_CSSD2101/src/main/java/com/university/restaurant/model/menu/MenuItem.java
@@ -2,12 +2,29 @@ package com.university.restaurant.model.menu;
 
 import java.util.List;
 
+/**
+ * @author Efua Itoadon-Umane
+ *
+ * This is the base class for all meal types to inherit from. 
+ * allows you to work with the name, description, price, availability and menu category
+ */
+
 public abstract class MenuItem {
     protected final String id, name, description;
     protected final double price;
     protected final MenuCategory category;
     protected final DietaryType dietaryType;
     protected boolean available;
+
+        /**
+     * 
+     * @param id for the newly added menu item
+     * @param name
+     * @param description of what the meal is 
+     * @param price
+     * @param category must choose from the types in the enum 
+     * @param dietaryType must choose from the types in the enum 
+     */
 
     protected MenuItem(String id, String name, String description, double price,
                        MenuCategory category, DietaryType dietaryType){

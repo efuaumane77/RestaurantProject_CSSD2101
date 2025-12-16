c50c000f7e5eb11167e708518f742433045dc0df
felda-del <felda@my.yorku.ca>
2025-12-15 23:55:19 -0500

    Add files via upload

---
+++ RestaurantProject_CSSD2101/Customer.java
@@ -0,0 +1,20 @@
+package com.university.restaurant.model.reservation;
+
+public final class Customer {
+    private final String name, phone, email;
+
+    public Customer(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer[%s : %s | %s]".formatted(name, phone, email);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
